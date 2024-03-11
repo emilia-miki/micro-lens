1d0b566e73afa6ab78628b2b61432ae10950f136
Emilia Miki <emiliamiki@icloud.com>
2024-03-11 22:42:50 +0200

    Implement iso and lens

--- lib/MicroLens.hs
+++ lib/MicroLens.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
 
 module MicroLens where
 
-import Prelude hiding (sum)
-import Data.Monoid
 import Control.Applicative
-import qualified Data.Traversable as T
+import Data.Monoid
+import Data.Traversable qualified as T
+import Prelude hiding (sum)
 
 ---------------------------------------------------------
 -- Some basic libraries
@@ -14,37 +15,37 @@ import qualified Data.Traversable as T
 class Profunctor p where
   dimap :: (a' -> a) -> (b -> b') -> (p a b -> p a' b')
   dimap f g = lmap f . rmap g
-  lmap ::  (a' -> a) -> (p a b -> p a' b)
+  lmap :: (a' -> a) -> (p a b -> p a' b)
   lmap f = dimap f id
-  rmap ::  (b -> b') -> (p a b -> p a b')
+  rmap :: (b -> b') -> (p a b -> p a b')
   rmap f = dimap id f
-  
-class Profunctor p => Choice p where
-  left'  :: p a b -> p (Either a c) (Either b c)
+
+class (Profunctor p) => Choice p where
+  left' :: p a b -> p (Either a c) (Either b c)
   right' :: p a b -> p (Either c a) (Either c b)
-  
+
 instance Profunctor (->) where
   dimap f g h = g . h . f
-  
+
 instance Choice (->) where
-  left'  f = either (Left . f) Right
+  left' f = either (Left . f) Right
   right' f = either Left (Right . f)
-  
+
 class Contravariant f where
   contramap :: (a' -> a) -> (f a -> f a')
 
 -- Control.Applicative.Const replicated here for your
 -- convenience
-newtype K b a = K { getK :: b } deriving Functor
+newtype K b a = K {getK :: b} deriving (Functor)
 
-instance Monoid b => Applicative (K b) where
+instance (Monoid b) => Applicative (K b) where
   pure _ = K mempty
   K e <*> K f = K (e <> f)
 
 instance Contravariant (K b) where
   contramap f (K b) = K b
 
-newtype Id a = Id { getId :: a } deriving Functor
+newtype Id a = Id {getId :: a} deriving (Functor)
 
 instance Applicative Id where
   pure = Id
@@ -52,42 +53,47 @@ instance Applicative Id where
 
 ---------------------------------------------------------
 -- The lens types you'll implement
-  
+
 -- | Optic is the general pattern for all other lens types.
 type Optic p f s t a b =
   p a (f b) -> p s (f t)
-  
+
 type Iso s t a b =
-  forall p f . (Profunctor p, Functor f) =>
+  forall p f.
+  (Profunctor p, Functor f) =>
   Optic p f s t a b
-  
+
 type Lens s t a b =
-  forall f . Functor f => 
+  forall f.
+  (Functor f) =>
   Optic (->) f s t a b
-  
+
 type Traversal s t a b =
-  forall f . Applicative f =>
+  forall f.
+  (Applicative f) =>
   Optic (->) f s t a b
-  
-type Fold s a = 
-  forall f . (Contravariant f, Applicative f) =>
+
+type Fold s a =
+  forall f.
+  (Contravariant f, Applicative f) =>
   Optic (->) f s s a a
 
 type Prism s t a b =
-  forall p f . (Choice p, Applicative f) =>
+  forall p f.
+  (Choice p, Applicative f) =>
   Optic p f s t a b
-    
+
 ---------------------------------------------------------
 ---------------------------------------------------------
 -- Todo
 
 -- | A lens focusing on the first element in a pair
 _1 :: Lens (a, x) (b, x) a b
-_1 = error "todo"
+_1 f s = fmap (,snd s) (f (fst s))
 
 -- | A lens focusing on the second element in a pair
 _2 :: Lens (x, a) (x, b) a b
-_2 = error "todo"
+_2 f s = fmap (fst s,) (f (snd s))
 
 -- | A function which takes a lens and looks through it.
 -- The type given is specialized to provide a hint as to
@@ -98,7 +104,7 @@ _2 = error "todo"
 -- view :: Lens s t a b -> (s -> a)
 -- @
 view :: Optic (->) (K a) s t a b -> (s -> a)
-view = error "todo"
+view l s = getK $ l K s
 
 -- | A function which takes a lens and a transformation function
 -- and applies that transformer at the focal point of the lens.
@@ -109,7 +115,7 @@ view = error "todo"
 -- over :: Lens s t a b -> (a -> b) -> (s -> t)
 -- @
 over :: Optic (->) Id s t a b -> (a -> b) -> (s -> t)
-over = error "todo"
+over l f s = getId $ l (pure . f) s
 
 -- | A function from a lens and a value which sets the value
 -- at the focal point of the lens. The type given has been
@@ -120,22 +126,22 @@ over = error "todo"
 -- set :: Lens s t a b -> b -> (s -> t)
 -- @
 set :: Optic (->) Id s t a b -> b -> (s -> t)
-set = error "todo"
+set l val s = getId $ l (\_ -> Id val) s
 
--- | A traversal which focuses on each element in any 
+-- | A traversal which focuses on each element in any
 -- Traversable container.
-elements :: T.Traversable f => Traversal (f a) (f b) a b
-elements = error "todo"
+elements :: (T.Traversable f) => Traversal (f a) (f b) a b
+elements f = sequenceA . fmap f
 
--- | A function which takes a Traversal and pulls out each 
--- element it focuses on in order. The type has been 
+-- | A function which takes a Traversal and pulls out each
+-- element it focuses on in order. The type has been
 -- specialized, as the others, but a more normal type might be
 --
 -- @
 -- toListOf :: Traversal s s a a -> (s -> [a])
 -- @
 toListOf :: Optic (->) (K (Endo [a])) s s a a -> (s -> [a])
-toListOf = error "todo"
+toListOf t s = error "todo"
 
 -- | A function which takes any kind of Optic which might
 -- be focused on zero subparts and returns Just the first
@@ -155,12 +161,12 @@ coerce = error "todo"
 
 -- | A Fold which views the result of a function application
 to :: (a -> b) -> Fold a b
-to f = error "todo"
+to f s t = contramap f (s (f t)) -- TODO: check?
 
 -- | A prism which focuses on the left branch of an Either
 _Left :: Prism (Either a x) (Either b x) a b
 _Left = error "todo"
-  
+
 -- | A prism which focuses on the right branch of an Either
 _Right :: Prism (Either x a) (Either x b) a b
 _Right = error "todo"
@@ -168,4 +174,6 @@ _Right = error "todo"
 -- | An iso which witnesses that tuples can be flipped without
 -- losing any information
 _flip :: Iso (a, b) (a, b) (b, a) (b, a)
-_flip = error "todo"
+_flip = dimap _flip' (fmap _flip')
+  where
+    _flip' (a, b) = (b, a)
